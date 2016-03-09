63663c96c9f4cd5e4f5d6fff647d872d793867cc
James Johnson <james.johnson@excellentingenuity.com>
2016-03-09 07:35:28 -0500

    added JWTException back in that was removed in merge

---
+++ src/Exceptions/JWTException.php
@@ -0,0 +1,20 @@
+<?php
+namespace eig\APIAuth\Exceptions;
+/**
+ * Class JWTException
+ * @package eig\APIAuth\Exceptions
+ */
+class JWTException extends \Exception
+{
+    /**
+     * JWTException constructor.
+     *
+     * @param string          $message
+     * @param int             $code
+     * @param \Exception|null $previous
+     */
+    public function __construct($message, $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
